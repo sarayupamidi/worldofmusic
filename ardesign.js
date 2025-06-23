ad56857c60e8b32f3832f6644e3f10e8dd015e2e
sarayupamidi <163379260+sarayupamidi@users.noreply.github.com>
2025-06-23 19:06:43 +0530

    Add files via upload

---
+++ ardesign.js
@@ -0,0 +1,40 @@
+var flag=1;
+function arplay1()
+{
+    var aud = document.getElementById("arterayaar");
+    if(flag==1)
+    {
+        aud.play();
+        flag=0;
+    }
+    else{
+        aud.pause();
+        flag=1;
+    }
+}
+function arplay2()
+{
+    var aud = document.getElementById("arphirbhi");
+    if(flag==1)
+    {
+        aud.play();
+        flag=0;
+    }
+    else{
+        aud.pause();
+        flag=1;
+    }
+}
+function arplay3()
+{
+    var aud = document.getElementById("ardevadevaohh");
+    if(flag==1)
+    {
+        aud.play();
+        flag=0;
+    }
+    else{
+        aud.pause();
+        flag=1;
+    }
+}
